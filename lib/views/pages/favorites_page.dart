3d594a0225fd43c42ea8c4e93278a91ad389d8fc
Mohammed Mohyialdin <mohamadmohyialdeen03@gmail.com>
2024-06-01 07:34:21 +0300

    update_commit

--- lib/views/pages/favorites_page.dart
+++ lib/views/pages/favorites_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:food_delivery/utils/app_colors.dart';
 
 import 'package:food_delivery/models/product_model.dart';
+
 class favpage extends StatefulWidget {
   const favpage({super.key});
 
@@ -12,35 +14,82 @@ class favpage extends StatefulWidget {
 class _favpageState extends State<favpage> {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemCount: favproduct.length,
-      itemBuilder: (_,index){
-        final pro=favproduct[index];
-        return Padding(padding:const EdgeInsets.symmetric(vertical: 5,horizontal: 15),
-        child:DecoratedBox(decoration: BoxDecoration(color: AppColors.white,
-                    borderRadius: BorderRadius.circular(16),
-                    border: Border.all(color: AppColors.primary)
+    return ListView.builder(
+        itemCount: favproduct.length,
+        itemBuilder: (_, index) {
+          final pro = favproduct[index];
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                  color: AppColors.white,
+                  borderRadius: BorderRadius.circular(16),
+                  border: Border.all(color: AppColors.primary)),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Image.network(
+                      pro.imgUrl,
+                      width: 70,
+                      height: 100,
                     ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal:10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-           Image.network(pro.imgUrl,width: 70,height: 100,),
-        
-          
-          SizedBox(height: 100,width: 100,child: Center(child: Text(pro.category.name,style: TextStyle(fontSize: 20,color: AppColors.primary,fontWeight: FontWeight.bold),))),
-          
-           Column(children: [
-            Text(pro.name, style:TextStyle(color: AppColors.primary,fontWeight: FontWeight.bold),),
-            SizedBox(height: 10,),
-             Text(pro.price.toString(),style: TextStyle(color: AppColors.primary,fontWeight: FontWeight.bold),),
-          
-           ],)
-          
-          ],),
-        ),
-        ) ,
-        );
-      });
+                    SizedBox(
+                        height: 100,
+                        width: 100,
+                        child: Center(
+                            child: Text(
+                          pro.category.name,
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: AppColors.primary,
+                              fontWeight: FontWeight.bold),
+                        ))),
+                    Column(
+                      children: [
+                        Text(
+                          pro.name,
+                          style: TextStyle(
+                              color: AppColors.primary,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          pro.price.toString(),
+                          style: TextStyle(
+                              color: AppColors.primary,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                    InkWell(
+                      onTap: () {
+                        setState(() {
+                          dummyProducts[int.parse(favproduct[index].id)-1].selfav = 0;
+                          favproduct.remove(favproduct[index]);
+                          
+                        });
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle, color: Colors.red),
+                        child: Padding(
+                          padding: const EdgeInsets.only(bottom: 13.0),
+                          child: Icon(
+                            Icons.minimize,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          );
+        });
   }
 }
